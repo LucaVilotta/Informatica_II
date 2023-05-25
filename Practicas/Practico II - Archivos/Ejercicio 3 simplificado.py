c1157d225e05396b7c3dce01c09f12d9971d1638
LucaVilotta <lucavilotta@hotmail.com>
2023-05-25 18:29:48 -0300

    Ejercicio 3 mas prolijo

--- Practicas/Practico II - Archivos/Ejercicio 3 simplificado.py
+++ Practicas/Practico II - Archivos/Ejercicio 3 simplificado.py
@@ -5,7 +5,9 @@ def wc(nomArch,opcion):
         arch = open(nomArchivo,'r',encoding='utf-8')
         #Contador para saber el número de linea
         contador_l = 0
+        #Contador para saber el número de caracteres
         contador_c = 0
+        #Variables op 3:
         palabrasList = []
         palabrasString = ''
         ## Op1
@@ -13,13 +15,13 @@ def wc(nomArch,opcion):
         for linea in arch:
             #Contador op 1:
             contador_l+=1
-            #Variables op 3:
+            
             
         ##Op2
             for c in linea:
                 if c!=" " and c!="\n":
                     contador_c+=1
-        #Op3 
+        ##Op3 
                     palabrasString += c
                 else:
                     palabrasList.append(palabrasString)
@@ -27,8 +29,10 @@ def wc(nomArch,opcion):
         # Si hay palabras después del último espacio o salto de línea en el archivo
         if palabrasString != '':
             palabrasList.append(palabrasString)
+        #Variable que almacena la cantidad de palabras del archivo
         cantidad_p = len(palabrasList)
-        #Op4
+        ##Op4
+        #Lista con las variables con la info requerida en cada opcion
         lista_info = [contador_l,contador_c,cantidad_p]
             
         ##Prints segun la Op:
